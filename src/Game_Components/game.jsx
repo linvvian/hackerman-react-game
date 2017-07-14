5c2604b1d42cd76667fd57bafdbf2a259e5ab536
drzl777 <andrew.zheng725@gmail.com>
2017-07-14 13:25:44 -0400

    made powerup disappear

--- src/Game_Components/game.jsx
+++ src/Game_Components/game.jsx
@@ -59,7 +59,7 @@ class Game extends Component {
          let tileSteppedOn = newBoard[state.player.y][state.player.x]
          console.log(tileSteppedOn, 'stepped on')
          if (tileSteppedOn === 2 || tileSteppedOn ===3) {
-           tileSteppedOn = 1
+           newBoard[state.player.y][state.player.x] = 1
          }
          const index = state.player.player - 1
          let allPlayers = [...this.state.players]
